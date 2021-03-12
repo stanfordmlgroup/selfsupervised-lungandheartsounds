53d702558b7cc7319509df5f5f1d0c371d396f6a
Pranav Sriram <pranav@DNa1ca4cb.SUNet>
2021-03-11 17:41:29 -0800

    Print statements

--- models/contrastive.py
+++ models/contrastive.py
@@ -31,6 +31,8 @@ import random
 
 
 def add_kd_loss(student_logits, teacher_logits, temperature):
+  print(student_logits.size)
+  print(teacher_logits.size)
   teacher_probs = Softmax(teacher_logits / temperature)
   kd_loss = torch.mean(temperature**2 * F.binary_cross_entropy_with_logits(student_logits / temperature, teacher_probs))
   return kd_loss
