38004af79d7137bd0eef86eef5cb34138dc4a253
Pranav Sriram <pranav@DNab421a7a.stanford.edu>
2021-03-14 14:38:36 -0700

    New save file

--- models/contrastive.py
+++ models/contrastive.py
@@ -518,7 +518,7 @@ class ContrastiveLearner(object):
             val_auc_score = roc_auc_score(val_true, val_pred)
 
             if best_dev_auc < val_auc_score:
-                lo.save_weights(model, os.path.join(log_dir, "student_distill_testing" + ".pt"))
+                lo.save_weights(model, os.path.join(log_dir, "student_pretrain_distill_with_new_teacher_fixed" + ".pt"))
                 best_dev_auc = val_auc_score
 
             num_teacher_params = count_parameters(teacher)
