ba5464149edd79e081f7519b5219637897e53b26
Pratham Soni <psoni@DN0a1f0a32.SUNet>
2021-10-28 09:30:55 -0700

    cleaning

--- data/test_set/rename.py
+++
@@ -1,8 +0,0 @@
-import os
-from glob import glob
-
-ids=glob(os.path.join(os.getcwd(),"*\\"))
-for id in ids:
-    wavs=glob(id+'*')
-    for idx, wav in enumerate(wavs):
-        os.rename(wav,id+str(idx)+".wav")
